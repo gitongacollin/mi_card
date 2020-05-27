ad4ff3fc250b4ff650e1404a11c99d7046104226
gitongacollin <gitongacollin@users.noreply.github.com>
2020-05-27 05:06:47 +0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.lightBlueAccent,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
@@ -37,7 +38,51 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.5,
                 ),
-              )
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.lightBlue.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    size: 50.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  title: Text(
+                    '+254734758121',
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade700,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 50.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    title: Text(
+                      'gitongacolin6@gmail.com',
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent.shade700,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 19,
+                      ),
+                    ),
+                  )),
             ],
           ),
         ),
